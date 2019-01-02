c4ae1404b2d2e8f19c35007d14dbc607d20993e9
michalbn <37307212+michalbn@users.noreply.github.com>
2019-01-02 05:15:45 +0200

    css+html to Welcome-page

--- my-sudoku-app/src/app/app.module.ts
+++ my-sudoku-app/src/app/app.module.ts
@@ -8,11 +8,10 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { SingleGameComponent } from './single-game/single-game.component';
 import { SudokuClassicGameComponent } from './sudoku-classic-game/sudoku-classic-game.component';
 import { FriendPageComponent } from './friend-page/friend-page.component';
-import { NewUserPageComponent } from './new-user-page/new-user-page.component';
-import { ForgotPasswordPageComponent } from './forgot-password-page/forgot-password-page.component';
+import { NewUserPageComponent } from './weclome-page/new-user-page/new-user-page.component';
 import { FriendsGamePageComponent } from './friends-game-page/friends-game-page.component';
-import { CompetitionGameComponent } from './competition-game/competition-game.component';
-import { CollaborationGameComponent } from './collaboration-game/collaboration-game.component';
+import { CompetitionGameComponent } from './friends-game-page/competition-game/competition-game.component';
+import { CollaborationGameComponent } from './friends-game-page/collaboration-game/collaboration-game.component';
 import { SudokuCompetitionGameComponent } from './sudoku-competition-game/sudoku-competition-game.component';
 import { SudokuCollaborationGameComponent } from './sudoku-collaboration-game/sudoku-collaboration-game.component';
 import { GradePageComponent } from './grade-page/grade-page.component';
@@ -27,7 +26,6 @@ import { SettingPageComponent } from './setting-page/setting-page.component';
     SudokuClassicGameComponent,
     FriendPageComponent,
     NewUserPageComponent,
-    ForgotPasswordPageComponent,
     FriendsGamePageComponent,
     CompetitionGameComponent,
     CollaborationGameComponent,
