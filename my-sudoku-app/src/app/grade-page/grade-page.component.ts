c43c0964d94f55b34b3dc2d1dcc5136b6867d992
michalbn <37307212+michalbn@users.noreply.github.com>
2019-01-26 20:32:37 +0200

    ngDoCheck - (Nested) Routes

--- my-sudoku-app/src/app/grade-page/grade-page.component.ts
+++ my-sudoku-app/src/app/grade-page/grade-page.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-grade-page',
   templateUrl: './grade-page.component.html',
   styleUrls: ['./grade-page.component.css']
 })
-export class GradePageComponent implements OnInit {
-
-  constructor() { }
+export class GradePageComponent implements OnInit,DoCheck {
+  gradePage = false;
+  path : string;
+  constructor(private router : Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+  }
+
+  ngDoCheck()//after any change
+  {
+    
+    this.path = this.router.routerState.snapshot.url
+    if (this.path == "/grade-page")
+    {
+      this.gradePage=true;
+    }
+    else
+    {
+      this.gradePage=false;
+    }
   }
 
 }
